d4f2c638fcf4f61b7b5740c4e8c41b58e9357e42
Arjun Krishna <arjun12345krishna@gmail.com>
2022-03-26 01:13:22 +0530

    Cards with Masonary Grid Layout

--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    var sample = 'Node.js + HBS Template'
-    res.render('home', { sample })
+    res.render('home')
 });
 
 module.exports = router;
