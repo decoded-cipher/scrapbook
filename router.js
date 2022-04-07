df404ce0820fea7d490376413eca3427f2ffe416
Arjun Krishna <arjun12345krishna@gmail.com>
2022-04-07 15:28:36 +0530

    Relative Time Successful

--- router.js
+++ router.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var ago = require('s-ago');
+
 var helper = require('./helper');
 
 router.get('/', (req, res) => {
     helper.getAllScraps().then((scraps) => {
-        
+
+        var scraps = Object.keys(scraps).map((key) => {
+            return scraps[key];
+        })
+
+        scraps.forEach(x => {
+            x.ago = ago(new Date(x.createdAt));
+            // console.log(x.ago);
+        });
+
         res.render('home', {scraps})
         // console.log(scraps);
 
